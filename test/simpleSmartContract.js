b7021968154898130d707a85cc610a11ae24c765
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-08-16 19:40:27 +0530

    Added simple smart contract and test file

---
+++ test/simpleSmartContract.js
@@ -0,0 +1,9 @@
+const SimpleSmartContract = artifacts.require('SimpleSmartContract');
+
+contract('SimpleSmartContract', (accounts) => {
+  it('should deploy smart contract properly', async () => {
+    const simpleSmartContract = await SimpleSmartContract.deployed();
+    console.log(simpleSmartContract.address);
+    assert(simpleSmartContract.address !== null);
+  });
+});
